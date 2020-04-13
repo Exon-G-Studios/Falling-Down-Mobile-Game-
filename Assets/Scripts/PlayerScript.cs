dd98b9a9a9f684a1f6e26c82e6a65c8477c4f880
gianleck <gianleck0@gmail.com>
2020-04-13 11:08:15 +0300

    Gereksiz şeyler
    Kank atmış olayım diye atıyorum burda bişi yok

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb2;
+    Vector3 vec;
+    int slot;
+    
+    
+        
+    
+}
