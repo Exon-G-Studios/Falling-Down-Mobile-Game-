28847690022ab00cbdcd051552ecc53c5aa42f1e
gianleck <gianleck0@gmail.com>
2020-04-25 16:29:36 +0300

    Player Script
    Player hareketi tamamlandı. Yarım kalmış bg scripti içinde.

---
+++ Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    public float scrollSpeed = 5;
+
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 20);
+        Debug.Log(newPos);
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public Rigidbody2D rb2;
-    Vector3 vec;
+    public Rigidbody2D rb2d;
+    public float speed = 5;
+    Vector2 vec;
     int slot;
-    
-    
-        
-    
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        vec = new Vector2(horizontal, vertical);
+
+        rb2d.velocity = vec * speed;
+    }
 }
