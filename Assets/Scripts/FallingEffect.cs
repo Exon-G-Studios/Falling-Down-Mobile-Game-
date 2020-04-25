c9d0c2a7c01e6ba835b9dec5538a5f8f7aeb7f01
GhostofHacker <45269095+GhostofHacker@users.noreply.github.com>
2020-04-25 16:32:34 +0300

    Kod ve Sprite
    Salsırma kodu ve bazı karekterlerin aninmasyon hızları ayarlanması.

---
+++ Assets/Scripts/FallingEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingEffect : MonoBehaviour
+{
+    protected GameObject player;
+    public bool isDisable = false;
+    public int SpacingAmount = 1;
+
+    void Start()
+    {
+        player = this.gameObject;
+        if (SpacingAmount >= 10)
+        {
+            SpacingAmount = 10;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
