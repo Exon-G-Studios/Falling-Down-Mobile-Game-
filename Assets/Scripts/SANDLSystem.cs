16df85fd1ed5ca639d927972e62b7d552422130b
GhostofHacker <45269095+GhostofHacker@users.noreply.github.com>
2020-05-21 17:26:22 +0300

    dataSytem and DynamicLevel Update 1
    Veri alma ve kaydetme çalışıyor. Şimdi Dinamik Level Seçme Ekleniyor. Menü Hataları gideriliyor.

--- Assets/Scripts/SANDLSystem.cs
+++ Assets/Scripts/SANDLSystem.cs
@@ -3,6 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//SANDL (Save and Load System Nasıl Kullanılır ?)
+//Veri oluşturma bölümünde istediğiniz veriyi class olarak ekledikten sonra veri etiketi bölümünden class adının önüne "_" işareti koyaraktan belirtin.
+//Veri Etiketini saveData fonksiyonuna ekleyin.
+//Veri Etiketini loadData fonsksiyonuna ekleyin.
+
+//Bir veriyi farklı bir ortamda ya da script'te erişmek ve değiştirmek için 
+//Her zaman SANDL Sisteminden veriye ulaşmadan önce loadData()'yı çağırın.
+//Sonra veri etiketine ulaşaraktan istediğiniz değeri değiştrin.
+//Veri değişikliğini kaydetmek için RunSANDL() fonskiyonu çağırın.
+
 public class SANDLSystem : MonoBehaviour
 {
     //Veri Etiketi Oluşturma Bölümü----------------------------------------------------------------------------------------------------------
@@ -12,6 +22,7 @@ public class SANDLSystem : MonoBehaviour
     public characterData _characterData;
     //Veriyi Değiştirmek İçin Üstekileri Kullan !
     //---------------------------------------------------------------------------------------------------------------------------------------
+    
     protected string filePath = @"Assets\Data\allData.json";
 
     //Veri Oluşturma Bölümü------------------------------------------------------------------------------------------------------------------
@@ -25,7 +36,16 @@ public class SANDLSystem : MonoBehaviour
     [System.Serializable]
     public class levelData      //Veriyi Değiştirmek İçin Bunu Kullanma //Ama Yeni Veri Tutucuyu Buraya Ekle (Variable)
     {
-        public int[][] levels;
+        //İlk [] değer oyun modunu göstermektedir.
+        //İkinci [] değer leveli göstermektedir.
+        //Üçüncü [] değer o levelin durumunu göstermektedir.
+        //1 Levelin açık olduğunu, 0 Levelin kapalı olduğunu söylemekte
+        public int[ , , ] levels = new int[3,3,1] {
+                                                    { { 1 }, { 0 }, { 0 } },    //GameMode 1
+                                                    { { 1 }, { 0 }, { 0 } },    //GameMode 2
+                                                    { { 1 }, { 0 }, { 0 } }     //GameMode 3
+                                                  };
+        //GameMode'a level eklendiğinde burdan o level eklenmelidir !!
     }
 
     [System.Serializable]
@@ -53,6 +73,8 @@ public class SANDLSystem : MonoBehaviour
         System.IO.File.WriteAllLines(filePath, data);
     }
 
+    //Veri Masası'na her eklediğin veri etiketi konumunueklemeyi unutma !
+
     //Veri Masası
     //-----------------------------------------------
     //Coin Data Erişimi: data[0]
@@ -65,6 +87,13 @@ public class SANDLSystem : MonoBehaviour
         _coindData = JsonUtility.FromJson<coinData>(data[0]);
         _levelData = JsonUtility.FromJson<levelData>(data[1]);
         _characterData = JsonUtility.FromJson<characterData>(data[2]);
+        //Data Alma Örneği
+        //veri etiketi = JsonUtility.FromJson<veri oluşturma class'ı>(veri etiketinin bulunduğu veri masası değeri);
+    }
+
+    public void RunSANDL(){
+        saveData();
+        loadData();
     }
 
 }
